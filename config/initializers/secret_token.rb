46dfd093b589eb7177db4c55b202fef208948911
Christopher Curtis <curtis@charliehq.com>
2013-08-16 15:51:19 -0500

    Fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TilesChallenge::Application.config.secret_key_base = '3e21e5e832d8e8bb25c8134b2fabcccaf77951b4900f956e4d4ae360ca1b8b569967cd5262c9a39559efaf6ba604ce3e25437cdd5647869db19edd1f18c23c66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TilesChallenge::Application.config.session_store :cookie_store, key: '_tiles-challenge_session'
