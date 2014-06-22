3d6ae8526dcb1f3efeea89e95830733dcee79841
Stephen Ausman <stephenausman@gmail.com>
2014-06-22 16:19:42 -0500

    Display tiles for the top 64 movie titles on IMDB.

--- app/controllers/tiles_controller.rb
+++ app/controllers/tiles_controller.rb
@@ -1,2 +1,14 @@
+require 'open-uri'
+
 class TilesController < ApplicationController
+  def index
+    # Performance could by improved by caching the top movies in a database
+    # and updating it periodically instead fetching the list on every request
+    imdb_top_250_uri = 'http://www.imdb.com/chart/top?ref_=nv_ch_250_4'
+    doc = Nokogiri::HTML open(imdb_top_250_uri)
+    @tiles = doc.css('.chart .titleColumn a')
+                .map(&:content)
+                .first(8 * 8)
+                .shuffle
+  end
 end
--- spec/controllers/tiles_controller_spec.rb
+++ spec/controllers/tiles_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
-describe TilesController do
+describe TilesController, type: :controller do
+  describe "GET tiles#index" do
+    it "assigns @tiles" do
+      get :index
+      expect(assigns(:tiles).length).to eq(8 * 8)
+    end
 
+    it "responds with a 200 HTTP status code" do
+      get :index
+      expect(response).to be_success
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 end
